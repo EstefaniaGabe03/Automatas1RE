6a70bd28c6b142e64eefdfc4305307fffd92bb43
Estefanía García Beltrán <al21760071@ite.edu.mx>
2024-02-16 19:42:32 -0800

    segundo intento

--- lector_json.js
+++ lector_json.js
@@ -7,7 +7,7 @@ const fS = require('fS');
 
 function impCarac(jsonFile){
     //lee el archivo JSON
-    const dato = fS.readFileSync(jsonFile,'utf8');
+    const dato = fS.readFileSync(jsonFile,'utf8');//lee el contenido del archivo
     const datoJson = JSON.parse(dato);
 
     //itera cada clave/valor en el objeto JSON
@@ -16,15 +16,23 @@ function impCarac(jsonFile){
         //propiedades directas del objeto. NUEVO METODO PARA MI
         if (datoJson.hasOwnProperty(key)){
             console.log("{");
-
+            //imprime los caracteres de la clave
             for(let i = 0; i < key.length; i++){
                 console.log(key[i]);
             }
             console.log(" ");
             console.log(":");
             console.log('"');
+            //imprime caracteres del valor
+            for (let i = 0; i < datoJson[key].length; i++){
+                console.log(datoJson[key][i]);
+            }
+            console.log('"');
+            console.log("}");
         }
 
     }
 }
 
+const jsonFile = 'prueba.json'//nombre del archo .json
+impCarac(jsonFile);
